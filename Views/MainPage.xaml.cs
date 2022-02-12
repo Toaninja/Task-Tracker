5f4f1c10f7d82253930953f553ffdc04cdf12255
Cosmin Ene <nationalsown@gmail.com>
2022-02-11 19:03:04 -0500

    Add project files.

---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Task_Tracker
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void navigationPanel_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            FrameNavigationOptions navigationOptions = new FrameNavigationOptions();
+            navigationOptions.TransitionInfoOverride = args.RecommendedNavigationTransitionInfo;
+            //if(args.IsSettingsSelected)
+            //{
+            //    contentFrame.Navigate(typeof(SettingsPage))
+            //}
+
+            string pageType = args.SelectedItemContainer.Tag.ToString();
+            switch (pageType)
+            {
+                case "HomePage":
+                    contentFrame.Navigate(typeof(HomePage));
+                    break;
+
+                case "GroupsPage":
+                    contentFrame.Navigate(typeof(GroupsPage));
+                    break;
+            }
+        }
+    }
+}
